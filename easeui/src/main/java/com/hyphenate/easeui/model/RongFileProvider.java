15fe6735902aa9baba35c8a8f5729e52c5bdd08c
Leon <646027023@qq.com>
2018-12-23 01:02:24 +0800

    适配安卓8.0

---
+++ easeui/src/main/java/com/hyphenate/easeui/model/RongFileProvider.java
@@ -0,0 +1,10 @@
+package com.hyphenate.easeui.model;
+
+
+
+/**
+ * Created by Leon on 2018/12/22.
+ * Functions: 创建一个空的 RongFileProvider 继承FileProvider 这里什么都不做在XML文件中配置
+ */
+public class RongFileProvider extends android.support.v4.content.FileProvider {
+}
